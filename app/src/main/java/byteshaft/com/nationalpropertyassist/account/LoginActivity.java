e547c5df37fd830dc04db01ea90f2ab33714b4b8
husnain446 <husnain446@gmail.com>
2016-06-02 10:19:21 +0500

    first commit

---
+++ app/src/main/java/byteshaft/com/nationalpropertyassist/account/LoginActivity.java
@@ -0,0 +1,18 @@
+package byteshaft.com.nationalpropertyassist.account;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import byteshaft.com.nationalpropertyassist.R;
+
+/**
+ * Created by husnain on 5/31/16.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.delegate_login);
+    }
+}
