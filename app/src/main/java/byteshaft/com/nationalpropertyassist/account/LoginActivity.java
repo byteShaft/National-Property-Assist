1c40b1e366a1f6af281fb9bb74147757405c70d5
husnain446 <husnain446@gmail.com>
2016-06-03 11:14:38 +0500

    register, login not functional

--- app/src/main/java/byteshaft/com/nationalpropertyassist/account/LoginActivity.java
+++ app/src/main/java/byteshaft/com/nationalpropertyassist/account/LoginActivity.java
@@ -1,7 +1,19 @@
 package byteshaft.com.nationalpropertyassist.account;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import byteshaft.com.nationalpropertyassist.R;
 
@@ -10,9 +22,85 @@ import byteshaft.com.nationalpropertyassist.R;
  */
 public class LoginActivity extends AppCompatActivity {
 
+    private Button mLoginButton;
+    private EditText mEmailAddress;
+    private EditText mPassword;
+    private TextView mSignUpText;
+    private CallbackManager callbackManager;
+//    private LoginButton faceBookButton;
+
+    private String mEmail;
+    private String mPasswordEntry;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.delegate_login);
+        callbackManager = CallbackManager.Factory.create();
+        mEmailAddress = (EditText) findViewById(R.id.email);
+        mPassword = (EditText) findViewById(R.id.password);
+        mLoginButton = (Button) findViewById(R.id.login);
+//        faceBookButton = (LoginButton) findViewById(R.id.login_button);
+//        faceBookButton.setReadPermissions("public_profile", "email", "user_friends");
+        mSignUpText = (TextView) findViewById(R.id.signup_text);
+
+        mSignUpText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegistrationActivity.class));
+            }
+        });
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+//        faceBookButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+//            @Override
+//            public void onSuccess(LoginResult loginResult) {
+//
+//            }
+//
+//            @Override
+//            public void onCancel() {
+//
+//            }
+//
+//            @Override
+//            public void onError(FacebookException error) {
+//
+//            }
+//        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private boolean validateEditText() {
+
+        boolean valid = true;
+        mPasswordEntry = mPassword.getText().toString();
+        mEmail = mEmailAddress.getText().toString();
+
+        if (mPasswordEntry.trim().isEmpty() || mPasswordEntry.length() < 3) {
+            mPassword.setError("invalid credentials");
+            valid = false;
+        } else {
+            mPassword.setError(null);
+        }
+
+        if (mEmail.trim().isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(mEmail).matches()) {
+            mEmailAddress.setError("invalid credentials");
+            valid = false;
+        } else {
+            mEmailAddress.setError(null);
+        }
+        return valid;
     }
 }
