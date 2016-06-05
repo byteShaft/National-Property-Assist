241f2242341cbc4eb4272221405bccde715e0c64
rb-shahid <shgr8rb@gmail.com>
2016-06-05 09:44:17 +0500

    change layout

--- app/src/main/java/byteshaft/com/nationalpropertyassist/AppGlobals.java
+++ app/src/main/java/byteshaft/com/nationalpropertyassist/AppGlobals.java
@@ -1,12 +1,19 @@
 package byteshaft.com.nationalpropertyassist;
 
 import android.app.Application;
+import android.content.Context;
 
 public class AppGlobals extends Application {
+
+    private static Context sContext;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        sContext = getApplicationContext();
     }
 
+    public static Context getContext() {
+        return sContext;
+    }
 }
