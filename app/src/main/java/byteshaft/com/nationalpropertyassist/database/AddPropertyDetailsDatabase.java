393b675f8bb91402a15c9f7886b250a496e4669c
husnain446 <husnain446@gmail.com>
2016-08-02 16:16:39 +0500

    add database packge

---
+++ app/src/main/java/byteshaft/com/nationalpropertyassist/database/AddPropertyDetailsDatabase.java
@@ -0,0 +1,124 @@
+package byteshaft.com.nationalpropertyassist.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by husnain on 8/1/16.
+ */
+public class AddPropertyDetailsDatabase extends SQLiteOpenHelper {
+
+    public AddPropertyDetailsDatabase(Context context) {
+        super(context, DatabaseConstants.DATABASE_NAME, null, DatabaseConstants.DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DatabaseConstants.TABLE_CREATE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS" + DatabaseConstants.TABLE_NAME);
+        onCreate(db);
+    }
+
+    public void createNewEntry(String address, int postalCode, int propertyCommercial, int propertyType,
+                               int ageOfProperty, int propertyId) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DatabaseConstants.ADDRESS_COLUMN, address);
+        values.put(DatabaseConstants.POSTAL_CODE_COLUMN, postalCode);
+        values.put(DatabaseConstants.PROPERTY_AGE_COLUMN, ageOfProperty);
+        values.put(DatabaseConstants.RESIDENTIAL_COMMERCIAL_COLUMN, propertyCommercial);
+        values.put(DatabaseConstants.PROPERTY_TYPE_COLUMN, propertyType);
+        values.put(DatabaseConstants.PROPERTY_ID_COLUMN, propertyId);
+        db.insert(DatabaseConstants.TABLE_NAME, null, values);
+        db.close();
+    }
+
+    public boolean deleteEntry(Integer id) {
+        SQLiteDatabase db = getWritableDatabase();
+        return db.delete(DatabaseConstants.TABLE_NAME, DatabaseConstants.ID_COLUMN + "=" + id, null) > 0;
+    }
+
+    public void updateEntries(
+            int id, String address, int postalCode, int propertyCommercial, int propertyType,
+            int ageOfProperty, int propertyId) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DatabaseConstants.POSTAL_CODE_COLUMN, postalCode);
+        values.put(DatabaseConstants.ADDRESS_COLUMN, address);
+        values.put(DatabaseConstants.PROPERTY_AGE_COLUMN, ageOfProperty);
+        values.put(DatabaseConstants.RESIDENTIAL_COMMERCIAL_COLUMN, propertyCommercial);
+        values.put(DatabaseConstants.PROPERTY_TYPE_COLUMN, propertyType);
+        values.put(DatabaseConstants.PROPERTY_ID_COLUMN, propertyId);
+        db.update(DatabaseConstants.TABLE_NAME, values, DatabaseConstants.ID_COLUMN + "=" + id, null);
+        Log.i("Database", "Updated.......");
+        db.close();
+    }
+
+    public ArrayList<Integer> getIdOfSaveProperty() {
+        SQLiteDatabase db = getReadableDatabase();
+        String query = "SELECT * FROM " + DatabaseConstants.TABLE_NAME;
+        Cursor cursor = db.rawQuery(query, null);
+        ArrayList<Integer> list = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            int id = cursor.getInt(
+                    cursor.getColumnIndex(DatabaseConstants.ID_COLUMN));
+            list.add(id);
+        }
+        db.close();
+        cursor.close();
+        return list;
+    }
+
+    public ArrayList<HashMap> getAllRecords() {
+        SQLiteDatabase db = getReadableDatabase();
+        String query = "SELECT * FROM " + DatabaseConstants.TABLE_NAME;
+        Cursor cursor = db.rawQuery(query, null);
+        ArrayList<HashMap> list = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            int unique_id = cursor.getInt(
+                    cursor.getColumnIndex(DatabaseConstants.ID_COLUMN));
+
+            String address = cursor.getString(
+                    cursor.getColumnIndex(DatabaseConstants.ADDRESS_COLUMN));
+            String postalCode = cursor.getString(
+                    cursor.getColumnIndex(DatabaseConstants.POSTAL_CODE_COLUMN));
+
+            String PropertyAge = cursor.getString(
+                    cursor.getColumnIndex(DatabaseConstants.PROPERTY_AGE_COLUMN));
+
+            String commercial = cursor.getString(
+                    cursor.getColumnIndex(DatabaseConstants.RESIDENTIAL_COMMERCIAL_COLUMN));
+
+            String propertyType = cursor.getString(
+                    cursor.getColumnIndex(DatabaseConstants.PROPERTY_TYPE_COLUMN));
+
+            String propertyId = cursor.getString(
+                    cursor.getColumnIndex(DatabaseConstants.PROPERTY_ID_COLUMN));
+
+            HashMap<String, String> hashMap = new HashMap<>();
+            hashMap.put("unique_id", String.valueOf(unique_id));
+            hashMap.put("address", address);
+            hashMap.put("postal_code", postalCode);
+            hashMap.put("Property_age", PropertyAge);
+            hashMap.put("commercial", commercial);
+            hashMap.put("property_type", propertyType);
+            hashMap.put("property_id", propertyId);
+            list.add(hashMap);
+        }
+        db.close();
+        cursor.close();
+        return list;
+    }
+}
---
+++ app/src/main/java/byteshaft/com/nationalpropertyassist/database/DatabaseConstants.java
@@ -0,0 +1,34 @@
+package byteshaft.com.nationalpropertyassist.database;
+
+/**
+ * Created by husnain on 8/1/16.
+ */
+public class DatabaseConstants {
+
+
+    public static final String DATABASE_NAME = "AddPropertyDetails.db";
+    public static final int DATABASE_VERSION = 1;
+    public static final String TABLE_NAME = "AddPropertyDatabase";
+    public static final String POSTAL_CODE_COLUMN = "POSTAL_CODE";
+    public static final String ADDRESS_COLUMN = "ADDRESS";
+    public static final String RESIDENTIAL_COMMERCIAL_COLUMN = "RESIDENTIAL_COMMERCIAL";
+    public static final String PROPERTY_TYPE_COLUMN = "PROPERTY_TYPE";
+    public static final String PROPERTY_AGE_COLUMN = "PROPERTY_AGE";
+    public static final String PROPERTY_ID_COLUMN = "PROPERTY_ID";
+    public static final String ID_COLUMN = "ID";
+
+    private static final String OPENING_BRACE = "(";
+    private static final String CLOSING_BRACE = ")";
+
+    public static final String TABLE_CREATE = "CREATE TABLE "
+            + TABLE_NAME
+            + OPENING_BRACE
+            + ID_COLUMN + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + ADDRESS_COLUMN + " TEXT,"
+            + POSTAL_CODE_COLUMN + " TEXT,"
+            + RESIDENTIAL_COMMERCIAL_COLUMN + " TEXT,"
+            + PROPERTY_TYPE_COLUMN + " TEXT,"
+            + PROPERTY_AGE_COLUMN + " TEXT,"
+            + PROPERTY_ID_COLUMN + " TEXT"
+            + CLOSING_BRACE;
+}
